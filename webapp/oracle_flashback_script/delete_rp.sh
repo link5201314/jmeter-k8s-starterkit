a2b8d8cbae88ae5c09857169a00d782cabc76583
isaacliu <link_5201314@hotmail.com>
2026-04-27 14:25:18 +0800

    feat: 新增 Oracle Flashback 腳本，包括創建、刪除、恢復還原點及進度查詢功能

---
+++ webapp/oracle_flashback_script/delete_rp.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+export ORACLE_BASE=/u01/app
+export ORACLE_HOME=/u01/app/oracle/product/23.0.0.0/dbhome_1
+export ORACLE_SID=CDBC1
+export ORACLE_TERM=xterm
+export PATH=$ORACLE_HOME/bin:$ORACLE_HOME/OPatch:/u01/app/xag/bin:/u01/app/ogg:$PATH:/usr/sbin
+export LD_LIBRARY_PATH=$ORACLE_HOME/lib:/lib:/usr/lib
+export THREADS_FLAG=native
+export CLASSPATH=$ORACLE_HOME/JRE:$ORACLE_HOME/jlib:$ORACLE_HOME/rdbms/jlib:$ORACLE_HOME/network/jlib
+export TEMP=/tmp
+export TMPDIR=/tmp
+export ORA_NLS33=$ORACLE_HOME/nls/data
+export NLS_LANG=AMERICAN_AMERICA.AL32UTF8
+
+cdate=`date +%Y%m%d%H%M%S`
+
+while getopts "p:r:" opt; do
+  case $opt in
+    p)
+      pdbn=$OPTARG
+      ;;
+    r)
+      rpn=$OPTARG
+      ;;
+    \?)
+      echo "Incorrect Option :  -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+
+pdb=`echo ${pdbn} |tr '[a-z]' '[A-Z]'`
+rp_name=`echo ${rpn} |tr '[a-z]' '[A-Z]'`
+
+
+
+drop_rp_sql(){
+local f_pdb=$1
+local f_rp=$2
+
+sqlplus -s "/ as sysdba" << EOF
+set feedback off;
+set heading off;
+DROP RESTORE POINT ${f_rp} FOR PLUGGABLE DATABASE ${f_pdb};
+exit;
+EOF
+}
+
+
+del_rp_ck(){
+local f_pdb=$1
+local f_rp=$2
+
+rp_1=`sqlplus -s "/ as sysdba" << EOF
+set feedback off;
+set heading off;
+select count(*) from v\\$restore_point where name='$f_rp' and con_id=(select con_id from v\\$pdbs where name='${pdb}' );
+exit;
+EOF`
+
+if [ $rp_1 == 0 ]
+   then
+       echo "DROP PDB: ${pdb} restore point : ${rp_name} successfully !"
+else
+       echo "DROP PDB: ${pdb} restore point : ${rp_name} fail ! "
+fi
+
+
+}
+
+ck_rp=`sqlplus -s "/ as sysdba" << EOF
+set feedback off;
+set heading off;
+select name from v\\$restore_point where name='${rp_name}' con_id=(select con_id from v\\$pdbs where name='${pdb}' );
+exit;
+EOF
+` 
+count_rp=`echo ${ck_rp} | wc -w`
+
+if [ ${count_rp} == 0 ]
+then 
+    echo "No Restore Poinrt to delete"
+    exit 1
+fi
+
+drop_rp_sql $pdb $rp_name
+del_rp_ck $pdb $rp_name
+   
---
+++ webapp/oracle_flashback_script/fb_process.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+export ORACLE_BASE=/u01/app
+export ORACLE_HOME=/u01/app/oracle/product/23.0.0.0/dbhome_1
+export ORACLE_SID=CDBC1
+export ORACLE_TERM=xterm
+export PATH=$ORACLE_HOME/bin:$ORACLE_HOME/OPatch:/u01/app/xag/bin:/u01/app/ogg:$PATH:/usr/sbin
+export LD_LIBRARY_PATH=$ORACLE_HOME/lib:/lib:/usr/lib
+export THREADS_FLAG=native
+export CLASSPATH=$ORACLE_HOME/JRE:$ORACLE_HOME/jlib:$ORACLE_HOME/rdbms/jlib:$ORACLE_HOME/network/jlib
+export TEMP=/tmp
+export TMPDIR=/tmp
+export ORA_NLS33=$ORACLE_HOME/nls/data
+export NLS_LANG=AMERICAN_AMERICA.AL32UTF8
+
+cdate=`date +%Y%m%d%H%M%S`
+
+while getopts "p:r:" opt; do
+  case $opt in
+    p)
+      pdbn=$OPTARG
+      ;;
+    r)
+      rpn=$OPTARG
+      ;;
+    \?)
+      echo "Incorrect Option :  -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+
+pdb=`echo ${pdbn} |tr '[a-z]' '[A-Z]'`
+rp_name=`echo ${rpn} |tr '[a-z]' '[A-Z]'`
+
+
+
+rp_1=`sqlplus -s "/ as sysdba" << EOF
+set feedback off;
+set heading off;
+select count(*) from v\\$session_longops 
+where con_id=(select con_id from v\\$pdbs where name='${pdb}') and opname like '%Flashback%';
+exit;
+EOF`
+
+
+if [ $rp_1 == 0 ]
+then
+    echo "No Restore process or restore complete"
+    #echo "C"
+    exit 1
+fi
+
+
+rp_2=`sqlplus -s "/ as sysdba" << EOF
+set feedback off;
+set heading off;
+select round(sofar/totalwork *100,2) as complete from v\\$session_longops
+where con_id=(select con_id from v\\$pdbs where name='${pdb}') and opname like '%Flashback%';
+exit;
+EOF`
+
+echo "PDB : ${pdb} => Restore $rp_2 % complete"
+#echo "R"
+
+   
